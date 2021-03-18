e96019d1e1311cac85ab18edc773c063130d0868
Nassim <nassim.haddad.pro@gmail.com>
2021-03-18 13:10:44 +0100

    Post: CRUD comments

--- controllers/postController.js
+++ controllers/postController.js
@@ -10,7 +10,6 @@ module.exports.createPost = async (req, res) => {
         likers: [],
         comments: [],
     })
-    console.log(newPost);
     try{
         const post = await newPost.save();
         console.log(post);
@@ -24,7 +23,7 @@ module.exports.readPost = (req, res) => {
     PostModel.find((err, docs) => {
         if(!err) return res.send(docs)
         else console.log('Error to get data : ' + err);
-    })
+    }).sort({ createdAt: -1 })
 }
 
 module.exports.updatePost = (req, res) => {
@@ -62,23 +61,23 @@ module.exports.likePost = async (req, res) => {
     try{
         await PostModel.findByIdAndUpdate(
             req.params.id,
-    {
+            {
                 $addToSet: { likers: req.body.id }
             },
-    { new: true },
-    (err, docs) => {
+            { new: true },
+            (err, docs) => {
                 if(err) return res.status(400).send(err);
             }
         );
 
         await UserModel.findByIdAndUpdate(
             req.body.id,
-    {
+            {
                 $addToSet: {likes: req.params.id}
             },
-    { new: true },
-    (err, docs) => {
-                if(!err) res.send(docs);
+            { new: true },
+            (err, docs) => {
+                if(!err) return res.send(docs);
                 else return res.status(400).send(err);
             }
         );
@@ -110,10 +109,89 @@ module.exports.unlikePost = async (req, res) => {
             },
             { new: true },
             (err, docs) => {
-                if(!err) res.send(docs);
+                if(!err) return res.send(docs);
                 else return res.status(400).send(err);
             }
         );
+    } catch (err) {
+        return res.status(400).send(err);
+    }
+}
+
+module.exports.commentPost = (req, res) => {
+    if(!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown : '+ req.params.id);
+
+    try{
+        return PostModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $push: {
+                    comments: {
+                        commenterId: req.body.commenterId,
+                        commenterPseudo: req.body.commenterPseudo,
+                        text: req.body.text,
+                        timestamp: new Date().getTime()
+                    }
+                }
+            },
+            { new: true },
+            (err, docs) => {
+                if(!err) return res.send(docs);
+                else return res.status(400).send(err);
+            }
+        )
+
+    } catch (err) {
+        return res.status(400).send(err)
+    }
+}
+
+module.exports.editCommentPost = (req, res) => {
+    if(!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown : '+ req.params.id);
+
+    try{
+        return PostModel.findById(
+            req.params.id, // Post ID
+            (err, docs) => {
+                // console.log(docs.comments);
+                const theComment = docs.comments.find((comment) => comment._id.equals(req.body.commentId)) // Comment ID
+                if(!theComment) return res.status(404).send('Comment not found');
+                theComment.text = req.body.text;
+
+                return docs.save((err) => {
+                    if(!err) return res.status(200).send(docs);
+                    else return res.status(500).send(err);
+                })
+            }
+        )
+    } catch (err) {
+        return res.status(400).send(err);
+    }
+}
+
+module.exports.deleteCommentPost = async (req, res) => {
+    if(!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown : '+ req.params.id);
+
+    try{
+        return PostModel.findByIdAndUpdate(
+            req.params.id,
+            {
+                $pull: {
+                    comments: {
+                        _id: req.body.commentId,
+                    }
+                }
+            },
+            {new: true},
+            ((err, docs) => {
+                if(!err) return res.send(docs);
+                else return res.status(400).send(err)
+            })
+
+        )
     } catch (err) {
         return res.status(400).send(err);
     }

24c2d34cc932f3778cc6b36b6f3ad1f9d2481078
Nassim <nassim.haddad.pro@gmail.com>
2021-03-23 18:20:25 +0100

    Front end: error handling for profile and post

--- client/src/actions/postAction.js
+++ client/src/actions/postAction.js
@@ -13,6 +13,9 @@ export const ADD_COMMENT = "ADD_COMMENT";
 export const EDIT_COMMENT = "EDIT_COMMENT";
 export const DELETE_COMMENT = "DELETE_COMMENT";
 
+// errors
+export const GET_POST_ERROR = "GET_POST_ERRORS";
+
 export const getPosts = (num) => {
   return (dispatch) => {
     return axios
@@ -59,9 +62,9 @@ export const createPost = (data) => {
       .post(`${process.env.REACT_APP_API_URL}api/post/`, data)
       .then((res) => {
         if (res.data.errors) {
-          // dispatch({ type: GET_POST_ERRORS, payload: res.data.errors });
+          dispatch({ type: GET_POST_ERROR, payload: res.data.errors });
         } else {
-          // dispatch({ type: GET_POST_ERRORS, payload: "" });
+          dispatch({ type: GET_POST_ERROR, payload: "" });
         }
       });
   }

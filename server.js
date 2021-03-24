f4b35beca9289974dc52e422ecc05e5ea0276900
Nassim <nassim.haddad.pro@gmail.com>
2021-03-24 19:09:21 +0100

    cors test

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 
 const corsOptions = {
     origin: process.env.CLIENT_URL,
+    credentials: true,
     'allowedHeaders': ['sessionId', 'Content-Type'],
     'exposedHeaders': ['sessionId'],
     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
